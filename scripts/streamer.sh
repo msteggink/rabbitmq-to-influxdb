57a4f37d9c7ec1412ad1c9e106033a5f7b429480
Matthew Steggink <matthew@msteggink.com>
2017-01-17 08:45:09 +0100

    added additional error handling

--- scripts/streamer.sh
+++ scripts/streamer.sh
@@ -15,25 +15,31 @@ USER="rabbit"
 PASS="rabbit"
 INFLUXDB_HOST="influxdb.example.com"
 INFLUXDB_DB="telegraf"
-
-trap ctrl_c INT
+LOCKFILE=/var/run/streamer
 
 function ctrl_c() {
-  rm -f /var/run/streamer
+  rm -f $LOCKFILE
   exit 0
 }
 
 
-if [ -f /var/run/streamer ]; then
+if [ -f $LOCKFILE ]; then
   echo "Lockfile present"
+  echo "Please check why. Submit the last metrics and remove the lockfile $LOCKFILE"
+  echo "curl -s -q -i -XPOST \"http://$INFLUXDB_HOST:8086/write?db=$INFLUXDB_DB\" --data-binary @$TMPDIR/linefile"
+  exit 1
+fi
+
+if [ -f $TMPDIR/linefile ]; then
+  echo "Possible unsubmitted metrics found."
+  echo "curl -s -q -i -XPOST \"http://$INFLUXDB_HOST:8086/write?db=$INFLUXDB_DB\" --data-binary @$TMPDIR/linefile"
   exit 1
 fi
 
 touch /var/run/streamer
-
 while [ true ] ;do
   rm -f /$TMPDIR/linefile
-  /usr/local/bin/rabbitmq-dump-queue -uri "amqp://$USER:$PASS@$RABBITMQHOST/$VHOST" -max-messages=$MESSAGES -output-dir $TMPDIR -queue=$QUEUE -ack=true 2>&1>/dev/null
+  /usr/local/bin/rabbitmq-dump-queue -uri "amqp://$USER:$PASS@$RABBITMQHOST/$VHOST" -max-messages=$MESSAGES -output-dir $TMPDIR -queue=$QUEUE --ack=true 2>&1 > /dev/null
   for file in `seq -w 0 $MESSAGES`; do
     if [ -f /$TMPDIR/msg-00$file ]; then
       cat $TMPDIR/msg-00$file >> $TMPDIR/linefile
@@ -41,11 +47,18 @@ while [ true ] ;do
     fi
   done
   if [ -f $TMPDIR/linefile ]; then
-    echo "Submitting `wc -l $TMPDIR/linefile `"
+    echo -n "Submitting `wc -l $TMPDIR/linefile ` lines... "
     rm -r $TMPDIR/msg-*
-    curl -q -i -XPOST "http://$INFLUXDB_HOST:8086/write?db=$INFLUXDB_DB" --data-binary @$TMPDIR/linefile
+    RT=`curl -s -w "%{http_code}" -q -i -XPOST "http://$INFLUXDB_HOST:8086/write?db=$INFLUXDB_DB" --data-binary @$TMPDIR/linefile -o /dev/null`
+    if [ $RT -ne 204 ]; then
+      echo "Unexpected response, aborting."
+      exit 1
+    else
+      rm -f $TMPDIR/linefile
+      echo " done"
+    fi
   else
     echo "No messages, sleeping"
-    sleep 10
+    sleep 30
   fi
 done
